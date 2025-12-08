a3896844833ee35afc08b97a6f4025ff44abe7b3
ArdenB <arden.burrell@gmail.com>
2025-12-09 10:35:33 +1100

    add initial implementation of the basic coding training script

---
+++ S01_BrynTraining.py
@@ -0,0 +1,31 @@
+"""
+Py script to Teach Basic Coding
+"""
+
+# ==============================================================================
+
+__title__ = "Training Script: Basic Coding"
+__author__ = "Arden Burrell"
+__version__ = "v1.0(22.05.2025)"
+__email__ = "arden.burrell@sydney.edu.au"
+
+
+# ==============================================================================
+
+import os
+import sys
+import argparse
+import pathlib
+import git
+# from git import exc as git_exc
+
+# ==============================================================================
+# ========== Import packages ==========
+import numpy as np
+import pandas as pd
+import yaml
+# import ipdb
+
+# ==============================================================================
+
+
