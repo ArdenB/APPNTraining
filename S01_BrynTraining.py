60c06f09191026d559a90d59dd8f000fe6b73a01
BrynWC <brynw.collyer@gmail.com>
2025-12-09 11:38:35 +1100

    Add data visualization for gamma data analysis
    - Imported matplotlib and seaborn for plotting.
    - Loaded gamma data from a CSV file with a dynamic filename.
    - Converted 'UtcTime' to a pandas datetime format and stored it in a new 'Time' column.
    - Created a line plot to visualize 'TOTAL_GMM [cps]' over time.
    - Added a regression plot to analyze the relationship between 'Thorium[cps]' and 'Uranium[cps]'.

--- S01_BrynTraining.py
+++ S01_BrynTraining.py
@@ -25,7 +25,42 @@ import numpy as np
 import pandas as pd
 import yaml
 # import ipdb
+# ========== Import plotting packages ==========
+
+import matplotlib.pyplot as plt
+import seaborn as sns       
+
 
 # ==============================================================================
+randomstr = "20251110"
+gamma_csv_path =f"MOLE_DATA/Gamma_{randomstr}.csv"
+#df = pd.read_csv("MOLE_DATA/Gamma_20251110.csv")
+gamma_df = pd.read_csv(gamma_csv_path,  encoding='Latin-1', skiprows=2 ,index_col=False) 
+# print(gamma_df)
+
+#===============================================================================
+
+# Convert UtcTime column to pandas datetime
+# gamma_df['UtcTime'] = pd.to_datetime(gamma_df['UtcTime'], unit='s', utc=True)
+gamma_df['Time'] = pd.to_datetime(gamma_df['UtcTime'], unit='s', utc=True)
+print(gamma_df["Time"])
+# gamma_df["Time"] = gamma_df["UtcTime"]
+
+# ================================================================================
+# Plot time vs TOTAL_GMM [cps] using seaborn
+df_subset = gamma_df[['Time', 'TOTAL_GMM [cps]','Thorium[cps]', 'Uranium[cps]', 'Potassium[cps]']]
+breakpoint()
+
+plt.figure(figsize=(12, 6))
+sns.lineplot(data=df_subset, x='Time', y='TOTAL_GMM [cps]')
+plt.title('TOTAL_GMM [cps] over Time')
+plt.xlabel('Time (UTC)')
+plt.ylabel('TOTAL_GMM [cps]')
+plt.xticks(rotation=45)
+plt.tight_layout()
+# plt.show()
+
+sns.regplot(data=df_subset, x="Thorium[cps]", y="Uranium[cps]", )
+plt.show()
 
 
